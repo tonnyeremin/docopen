6bdd786166780c0e334840561b9241b2cfa0ae39
Tonny <an.eremin@gmail.com>
2020-09-26 15:58:10 +0200

    Main page layout

---
+++ src/DocOpen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DocOpen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
